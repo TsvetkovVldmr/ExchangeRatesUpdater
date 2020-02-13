b2f56055ee6854d370ac499eb23abba9d3ccb151
Tsvetkov Vladimir <tsvetkov.vldmr@gmail.com>
2020-02-13 16:25:16 +0300

    Добавлена библиотека, которая будет отвечать за выгрузку данных с сайта ЦБ. Добавлен класс валюты

---
+++ ExchangeRatesUpdater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExchangeRatesUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
