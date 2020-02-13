93ffd446ccc11c11038e908fb30cb770794b8a7b
Tsvetkov Vladimir <tsvetkov.vldmr@gmail.com>
2020-02-13 16:26:28 +0300

    Добавлена библиотека, которая будет отвечать за выгрузку данных с сайта ЦБ. Добавлен класс валюты

---
+++ ExchangeRates/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeRates
+{
+    public struct Currency
+    {
+        public string ID { get; private set; }
+        public int NumCode { get; private set; }
+        public string CharCode { get; private set; }
+        public int Nominal { get; private set; }
+        public string Name { get; private set; }
+        public double Value { get; private set; }
+
+    }
+}
