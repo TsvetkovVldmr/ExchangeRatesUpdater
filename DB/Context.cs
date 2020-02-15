1740cac941ec470876515985883d80a2563f2757
Tsvetkov Vladimir <tsvetkov.vldmr@gmail.com>
2020-02-15 16:29:26 +0300

    Восстановлена строка подключения

--- DB/Context.cs
+++ DB/Context.cs
@@ -23,7 +23,7 @@ namespace DB
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-TEUJ96G;Initial Catalog=Context;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");    
             }
         }
 
